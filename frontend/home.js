d6c332a94ea2577573b9d3d5129126c157dbc502
marcosmanfre <marcosmanfre@hotmail.com>
2023-04-23 18:36:45 -0300

    home

--- frontend/home.js
+++ frontend/home.js
@@ -1,1 +1,60 @@
-const home={template:`<h1>Bem vindo ao sistema de gerencimanto de Alunos</h1>`}
+const home={template:`
+<header class="bg-dark py-5">
+  <div class="container px-5">
+      <div class="row gx-5 align-items-center justify-content-center">
+          <div class="col-lg-8 col-xl-7 col-xxl-6">
+              <div class="my-5 text-center text-xl-start">
+                  <h1 class="display-5 fw-bolder text-white mb-3">Gerenciador de Alunos</h1>
+                  <p class="lead fw-normal text-white-50 mb-4">O nosso sistema gerenciador de alunos oferece uma plataforma fácil de usar para manter informações de estudantes atualizadas e acessíveis de qualquer lugar.</p>
+                  <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                 
+                  </div>
+              </div>
+          </div>
+          <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="https://cdn.pixabay.com/photo/2016/11/13/04/52/statistic-1820320_960_720.png" alt="..." /></div>
+      </div>
+  </div>
+</header>
+<section class="py-5" id="features">
+  <div class="container px-5 my-5">
+      <div class="row gx-5">
+          <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0"> O sistema de gestão escolar que você precisa!</h2></div>
+          <div class="col-lg-8">
+              <div class="row gx-5 row-cols-1 row-cols-md-2">
+                  <div class="col mb-5 h-100">
+                      <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
+                      <h2 class="h5">Sistema para Educação Básica - Instituições privadas</h2>
+                      <p class="mb-0">Tenha o melhor acompanhamento pedagógico com o sistema de gestão escolar que se adapta às particularidades do seu modelo de ensino e mantenha sua escola conectada com os alunos, pais e responsáveis.</p>
+                  </div>
+                  <div class="col mb-5 h-100">
+                      <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
+                      <h2 class="h5">Sistema para Escola de Idiomas e Cursos Livres</h2>
+                      <p class="mb-0">Conte com o Sponte para conquistar mais matrículas, a partir da gestão de contatos com alunos interessados, e para reduzir a inadimplência escolar.</p>
+                  </div>
+                  <div class="col mb-5 mb-md-0 h-100">
+                      <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                      <h2 class="h5">Sistema para Ensino Técnico e Profissionalizante</h2>
+                      <p class="mb-0">Reduza o índice de inadimplência da sua escola, elimine planilhas manuais, automatize processos financeiros.</p>
+                  </div>
+                  <div class="col h-100">
+                      <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                      <h2 class="h5">Eficiência na gestão educacional para transformar a educação</h2>
+                      <p class="mb-0">Com o sistema de gestão escolar, sua escola será mais produtiva e sua equipe terá mais tempo para se concentrar no que realmente importa: ensinar!</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>
+
+
+<footer class="bg-dark py-4 mt-auto">
+  <div class="container px-5">
+      <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+          <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Marcos Manfre 2023</div></div>
+          <div class="col-auto">
+          </div>
+      </div>
+  </div>
+</footer>
+`}
